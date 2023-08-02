bacd719bb6ea8d30784b826f74b32b9c6594295b
DESKTOP-3KE791L\user <lshe543211@yahoo.com.tw>
2023-08-03 00:21:36 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Alert } from "@mui/material";
+
+const App = () => {
+  return (
+    <div className="flex">
+      <div className="text-sm">123</div>
+      <div>456</div>
+      <div>789</div>
+      <Alert>45</Alert>
+    </div>
+  );
+};
+
+export default App;
